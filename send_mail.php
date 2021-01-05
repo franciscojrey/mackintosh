4a0cea5be1aee7534ebc84be2950b19b948850ab
FranciscoRey <franciscorey98@gmail.com>
2021-01-05 18:35:26 -0300

    cambios requeridos

--- send_mail.php
+++ send_mail.php
@@ -3,11 +3,12 @@
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $subject = $_POST['subject'];
+    $telefono = $_POST['telefono']
     $mailFrom = $_POST['email'];
     $message = $_POST['message'];
 
     $mailTo = "soporte@adasoftware.com.ar";
-    $txt = "Email enviado desde el sitio web Adasoftware.com.ar \n\nNombre: ".$name.".\n\n"."Mail: ".$mailFrom.".\n\n"."Mensaje: ".$message;
+    $txt = "Email enviado desde el sitio web www.rmackintosh.com.ar \n\n Nombre: ".$name.".\n\n"."Teléfono: ".$telefono.".\n\n"."Mail: ".$mailFrom.".\n\n"."Mensaje: ".$message;
     
     mail($mailTo, $subject, $txt);
     header("Location: message_sended.html");
